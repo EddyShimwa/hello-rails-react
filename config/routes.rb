93602ef550a311872a2a05af721f6829ca572bee
EddyShimwa <shedpray16@gmail.com>
2023-05-12 12:08:15 +0200

    last commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 # frozen_string_literal: true
-
 Rails.application.routes.draw do
   root 'home#index'
-
   namespace :api do
     namespace :v1 do
-      resources :greetings, only: [:index]
+      get 'greetings/index', to: 'greetings#index'
     end
   end
 end
